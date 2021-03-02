c9c8b3b343372300588159d653101e90b0252bd4
Stacy Read <stacy.read.007@gmail.com>
2021-03-01 23:53:53 -0600

    Added comments to Key

--- src/edu/wctc/Key.java
+++ src/edu/wctc/Key.java
@@ -35,12 +35,15 @@ public class Key {
     public String decrypt(BigInteger cipherText) {
         String message = "";
 
+        // Perform the decryption
         BigInteger decryptedNum = cipherText.multiply(d()).mod(n());
         System.out.printf("Decrypt: %d * %d (%d) %% %d = %d%n", cipherText, d(), cipherText.multiply(d()), n(), decryptedNum);
 
+        // Turn the BigInteger into a string of 1's and 0's by specifying radix of base 2
         String bits = decryptedNum.toString(2);
         System.out.printf("Decrypt: binary %s%n", bits);
 
+        // Ensure the string can be chopped even into groups of eight 1's and 0's
         while (bits.length() % 8 != 0) {
             bits = "0" + bits; // left pad with zeroes to ensure groups of 8
         }
@@ -48,13 +51,17 @@ public class Key {
         System.out.print("Decrypt: Processing byte chunks to characters... ");
         // Grab chunks of 8 bits and convert to an ASCII character
         for (int i = 0; i < bits.length(); i += 8) {
+            // Eight 1's and 0's
             String byteChunk = bits.substring(i, i + 8);
+            // Parse the string using radix base 2
             int decimal = Integer.parseInt(byteChunk, 2);
+            // Convert to a character
             String character = Character.toString(decimal);
             System.out.print(character + " ");
+            // Append to plaintext message
             message += character;
         }
-        System.out.println();
+        System.out.println(); // just a newline
 
         return message;
     }
@@ -67,9 +74,11 @@ public class Key {
     Encryption is performed with the public key.
      */
     public BigInteger encrypt(String message) throws KeySizeException {
+        // Turn string into array of bytes (numbers)
         byte[] bytes = message.getBytes(StandardCharsets.US_ASCII);
         StringBuilder binary = new StringBuilder();
 
+        // Do some bitwise operations to translate decimal to binary
         System.out.print("Encrypt: Processing characters to byte chunks...");
         for (byte b : bytes) {
             System.out.print(b + " ");
@@ -81,15 +90,21 @@ public class Key {
         }
         System.out.println();
 
+        // Print out the resulting binary string. 1's and 0's...
         System.out.printf("Encrypt: binary %s%n", binary.toString());
+
+        // Create a BigInteger from the string, specific radix of base 2
         BigInteger m = new BigInteger(binary.toString(), 2);
 
+        // Check if the message is too big for this key to encrypt
         if (m.compareTo(n()) > 0) {
             throw new KeySizeException(m, n());
         } else {
+            // Perform the encryption
             BigInteger result = m.multiply(e()).mod(n());
+            // Print out the calculations being done on the line above
             System.out.printf("Encrypt: %d * %d (%d) %% %d = %d%n", m, e(), m.multiply(e()), n(), result);
-
+            // Return encrypted string as a number
             return result;
         }
     }
